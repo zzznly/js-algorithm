f101819c5f194ddbd5a241cfa13ea6d1b9a6a90f
zzznly <zzznly@gmail.com>
2023-06-27 15:49:06 +0900

    최소값 구하기 수정

--- section01/최소값_구하기.js
+++ section01/최소값_구하기.js
@@ -2,15 +2,19 @@
 
 function solution(...args) {
   let answer,
-    min = Number.MAX_SAFE_INTEGER;
+    min = Number.MAX_SAFE_INTEGER; // 최소값 구할때 -> 안정적인 가장 큰 정수값을 할당해주는것도 좋다! (900719925474991...)
+
   for (let i = 0; i < args.length; i++) {
     if (args[i] < min) min = args[i];
   }
   answer = min;
   return answer;
-
-  // 다른 풀이 *
-  // return Math.min(...args);
 }
 
+// 다른 풀이 *
+// function solution(...args) {
+//     return Math.min(...args);
+// }
+
+// 출력
 console.log(solution(5, 3, 7, 11, 2, 15, 17)); // 2
