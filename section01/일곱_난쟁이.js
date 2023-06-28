b386e96f19d3f70284fd54126afbe2c21208495e
zzznly <zzznly@gmail.com>
2023-06-28 20:11:28 +0900

    일곱난쟁이 수정

--- section01/일곱_난쟁이.js
+++ section01/일곱_난쟁이.js
@@ -6,17 +6,21 @@
 // * 강의 풀이:
 function solution(arr) {
   let sum = arr.reduce((acc, cur) => acc + cur, 0);
+  let flag = 0;
 
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (sum - (arr[i] + arr[j]) === 100) {
         arr.splice(j, 1);
         arr.splice(i, 1);
+        flag = 1;
+        break; // 답이 발견되면 break 해서 벗어날것!
       }
     }
+    if (flag) break;
   }
   return arr;
 }
 
 // 출력 :
-console.log(solution([20, 7, 23, 19, 10, 15, 25, 8, 13])); // 20 7 23 19 10 8 13
+console.log(solution([22, 7, 21, 19, 10, 15, 25, 8, 13])); // 22 7 10 15 25 8 13
