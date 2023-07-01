b3782d4f89f1027d6848bef33b0d3a1ca4f9b4f3
zzznly <zzznly@gmail.com>
2023-07-01 23:52:57 +0900

    보이는 학생 수정

--- section02/보이는_학생.js
+++ section02/보이는_학생.js
@@ -7,14 +7,14 @@
 
 // 나의 풀이 1:
 function solution1(arr) {
-  return [arr[0], ...arr.filter((_, i) => arr[i] > arr[i - 1])].length;
+  return [...arr.filter((_, i) => arr[i] > arr[i - 1])].length;
 }
 
 // 강의 풀이
 function solution2(arr) {
   let answer = 1,
     max = arr[0];
-  for (let i = 1; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max) answer++;
     max = arr[i];
   }
