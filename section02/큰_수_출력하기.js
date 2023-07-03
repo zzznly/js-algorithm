0dd50a244c8e5a2479e0d7814409ce25bc8ca66f
zzznly <zzznly@gmail.com>
2023-07-03 23:23:32 +0900

    큰 수 출력하기 수정

--- section02/큰_수_출력하기.js
+++ section02/큰_수_출력하기.js
@@ -8,8 +8,8 @@ function solution1(arr) {
 
 // 나의 풀이 2:
 function solution2(arr) {
-  let answer = [];
-  answer.push(arr[0]);
+  let answer = [arr[0]];
+  //   answer.push(arr[0]);
 
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > arr[i - 1]) answer.push(arr[i]);
