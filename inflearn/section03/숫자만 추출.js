e73fa2dfc0c848db4b2249130721134c92b56c43
zzznly <zzznly@gmail.com>
2023-08-02 00:29:31 +0900

    [문자열탐색] 숫자만 추출

---
+++ inflearn/section03/숫자만 추출.js
@@ -0,0 +1,28 @@
+/*
+    Q. 문자와 숫자가 섞여있는 문자열이 주어지면 그 중 숫자만 추출하여 그 순서대로 자연수를 만듭니다.
+    만약 “tge0a1h205er”에서 숫자만 추출하면 0, 1, 2, 0, 5이고 이것을 자연수를 만들면 1205 이 됩니다.
+    추출하여 만들어지는 자연수는 100,000,000을 넘지 않습니다.
+*/
+
+// 나의 풀이:
+function solution1(str) {
+  let answer;
+
+  answer = +[...str].filter(v => !isNaN(+v)).join("");
+
+  return answer;
+}
+
+// 강의 풀이:
+function solution2(str) {
+  let answer = 0;
+
+  for (let x of str) {
+    if (!isNaN(x)) answer += x;
+  }
+
+  return parseInt(answer);
+}
+
+console.log(solution1("g0en2T0s8eSoft")); // 208
+console.log(solution2("g0en2T0s8eSoft")); // 208
