80e097124c7ea2f8ff17554f20a8310acecd1b17
zzznly <zzznly@gmail.com>
2023-08-23 19:54:40 +0900

    [완전탐색] 뒤집은소수 - 강의 풀이 추가

--- inflearn/04_완전 탐색/[완전탐색] 뒤집은 소수.js
+++ inflearn/04_완전 탐색/[완전탐색] 뒤집은 소수.js
@@ -30,5 +30,33 @@ function solution1(arr) {
   return answer;
 }
 
+// 강의 풀이
+function isPrime(num) {
+  if (num === 1) return false;
+  for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution2(arr) {
+  let answer = [];
+
+  for (let x of arr) {
+    let res = 0;
+
+    while (x) {
+      let t = x % 10;
+      res = res * 10 + t;
+      x = parseInt(x / 10);
+    }
+
+    if (isPrime(res)) answer.push(res);
+  }
+
+  return answer;
+}
+
 // 출력
 console.log(solution1([32, 55, 62, 20, 250, 370, 200, 30, 100])); // 23 2 73 2 3
+console.log(solution2([32, 55, 62, 20, 250, 370, 200, 30, 100])); // 23 2 73 2 3
